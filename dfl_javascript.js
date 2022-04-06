d228d7166fc8d3a3d4fb91c2857f0beb5c4d006d
Dave Levy <davelevy.eu@gmail.com>
2022-04-06 15:54:56 +0100

    Update dfl_javascript.js
    adds a working version of showmodal with event listners

--- dfl_javascript.js
+++ dfl_javascript.js
@@ -3,3 +3,25 @@ function toggle_text(id) {
     if(e.style.display != 'block') 
         e.style.display = 'block'; 
     else e.style.display = 'none'; }
+
+function showmodal (id,bid, cid) /* DIV, button, close class name */ {
+
+    var modal = document.querySelector(id);
+    var btn = document.querySelector(bid);
+    var span = document.querySelector(cid);
+
+    btn.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    span.onclick = function () { modal.style.display = "none"; }
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", function (event) {
+        //console.log(event.target);
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
+
